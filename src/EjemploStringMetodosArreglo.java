9ce8eb1b40eefb216a8984c1c6c568d2a135d451
gustavo177 <gtvadf177@hotmail.com>
2024-02-18 14:52:23 -0500

    Algunos métodos útiles para convertir un String en un Arreglo

---
+++ src/EjemploStringMetodosArreglo.java
@@ -0,0 +1,33 @@
+public class EjemploStringMetodosArreglo {
+    public static void main(String[] args) {
+
+        String trabalenguas = "trabalenguas";
+        System.out.println("trabalenguas.length() = " + trabalenguas.length());
+        System.out.println("trabalenguas.toCharArray() = " + trabalenguas.toCharArray());
+
+        char[] arreglo = trabalenguas.toCharArray();
+        int largo = arreglo.length;
+
+        for (int i = 0; i < largo; i++){
+            System.out.print(arreglo[i]);
+        }
+        System.out.println();
+        System.out.println("\ntrabalenguas = " + trabalenguas.split("a"));
+        String[] arreglo2 = trabalenguas.split("a");
+        int l =arreglo2.length;
+        for (int j = 0; j<l; j++){
+            System.out.println(arreglo2[j]);
+        }
+
+        String archivo = "alguna.imagen.pdf";
+        String[] archivoArr = archivo.split("\\."); // [.]
+
+        l=archivoArr.length;
+        System.out.println("largo l = " + l);
+        for (int j = 0; j<l; j++){
+            System.out.println(archivoArr[j]);
+        }
+        System.out.println("extensión = " + archivoArr[l-1]);
+
+    }
+}

9171f38f7de64e92d5ac0e32655428717391bfbe
gustavo177 <gtvadf177@hotmail.com>
2024-03-06 10:04:48 -0500

    La clase Math y el método random() para generar números aleatorios

---
+++ src/EjemploClaseMathRandom.java
@@ -0,0 +1,15 @@
+public class EjemploClaseMathRandom {
+    public static void main(String[] args) {
+        String[] colores = {"azul","amarillo","rojo","blanco","verde"};
+        double random = Math.random();
+        System.out.println("random = " + random);
+        random *= colores.length;
+
+        System.out.println("random = " + random);
+
+        random = Math.floor(random);
+        System.out.println("random = " + random);
+
+        System.out.println("colores[] = " + colores[(int)random]);
+    }
+}

eb4ecb61d32735d2368913b2f9a68eae694ce6d2
gustavo177 <gtvadf177@hotmail.com>
2024-03-06 10:11:10 -0500

    La clase Random de java util

--- src/EjemploClaseMathRandom.java
+++ src/EjemploClaseMathRandom.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class EjemploClaseMathRandom {
     public static void main(String[] args) {
         String[] colores = {"azul","amarillo","rojo","blanco","verde"};
@@ -11,5 +13,14 @@ public class EjemploClaseMathRandom {
         System.out.println("random = " + random);
 
         System.out.println("colores[] = " + colores[(int)random]);
+
+        Random randomObj = new Random();
+        int randomInt = 15 + randomObj.nextInt(25-15+1);
+        System.out.println("randomInt = " + randomInt);
+
+        randomInt = randomObj.nextInt(colores.length);
+        System.out.println("randomInt = " + randomInt);
+        System.out.println("colores = " + colores[randomInt]);
+        
     }
 }

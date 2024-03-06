0bad7b37b0dbc166a3851a7bb6c7820db74aed4a
gustavo177 <gtvadf177@hotmail.com>
2024-03-06 11:01:34 -0500

    agregamos "Debe ingresar argumentos parámetros!"

--- src/ArgumentosLineaComando.java
+++ src/ArgumentosLineaComando.java
@@ -1,7 +1,7 @@
 public class ArgumentosLineaComando {
     public static void main(String[] args) {
         if(args.length==0){
-            System.out.println("Debe ingresar argumentos!");
+            System.out.println("Debe ingresar argumentos parámetros!");
             System.exit(-1);
         }
         for (int i = 0; i < args.length; i++){
